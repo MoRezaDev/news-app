e418ea0881800d154bb5769b79a74b0b10328ea2
Mohammad Reza Akhshabi <MoReza.Dev@gmail.com>
2023-03-03 17:51:24 +0330

    .env file changed

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.BASE_URL,
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cle8ajeks04so01ufbf8p7718/master",
   cache: new InMemoryCache(),
 });
 
